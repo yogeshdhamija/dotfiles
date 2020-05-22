4b5b962c71f3225fc1101f0d950db07d4c40d73c
Yogesh Dhamija <ydhamija96@gmail.com>
2020-05-21 20:26:50 -0400

    cleaned up status check

--- .check_environment.sh
+++ .check_environment.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# VARIABLES:
-declare -a NOTES=(
-    "Welcome!"
-    "Here is some software integrated into this config (or just that I recommend)."
-)
 declare -a EXECS=(
     "nvim"
     "zsh"
@@ -50,33 +45,21 @@ declare -a ADDITIONAL2=(
     ""
 )
 
-# SCRIPT:
-noteslength=${#NOTES[@]}
 arraylength=${#EXECS[@]}
 
-echo "Cl9fICBfXyAgICAgICAgICAgICAgICAgICAgICAgICAgIF9fICAgXyAgICAgICAgICBfX19fX18gICAgICAgICAgICAgICBfX19fIF8gICAgICAgClwgXC8gL19fX18gICBfX19fIF8gX19fICAgX19fX18gLyAvXyAoIClfX19fXyAgIC8gX19fXy9fX19fICAgX19fXyAgIC8gX18vKF8pX19fXyBfCiBcICAvLyBfXyBcIC8gX18gYC8vIF8gXCAvIF9fXy8vIF9fIFx8Ly8gX19fLyAgLyAvICAgIC8gX18gXCAvIF9fIFwgLyAvXyAvIC8vIF9fIGAvCiAvIC8vIC9fLyAvLyAvXy8gLy8gIF9fLyhfXyAgKS8gLyAvIC8gKF9fICApICAvIC9fX18gLyAvXy8gLy8gLyAvIC8vIF9fLy8gLy8gL18vIC8gCi9fLyBcX19fXy8gXF9fLCAvIFxfX18vL19fX18vL18vIC9fLyAvX19fXy8gICBcX19fXy8gXF9fX18vL18vIC9fLy9fLyAgL18vIFxfXywgLyAgCiAgICAgICAgICAvX19fXy8gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgL19fX18vICAgCg==" | base64 --decode && echo ""
-
-for (( i=1; i<${noteslength}+1; i++ ));
-do
-    echo ${NOTES[$i-1]}
-    echo ""
-done
-
 echo ""
-echo "Already installed:" && echo ""
+echo "***            CONFIGURATION STATUS CHECK            ***"
+echo ""
+
+echo "Programs found in PATH:"
 for (( i=1; i<${arraylength}+1; i++ ));
 do
     if command -v ${EXECS[$i-1]} > /dev/null ; then
         echo '    -' ${NAMES[$i-1]} '('${EXECS[$i-1]}')'
-        [[ ! -z ${ADDITIONAL1[$i-1]} ]] && echo '        ' ${ADDITIONAL1[$i-1]}
-        [[ ! -z ${ADDITIONAL2[$i-1]} ]] && echo '        ' ${ADDITIONAL2[$i-1]}
-        echo ""
     fi
 done
 
-echo ""
-
-echo "Not found:" && echo ""
+echo "Programs not found in PATH:" 
 for (( i=1; i<${arraylength}+1; i++ ));
 do
     if ! command -v ${EXECS[$i-1]} > /dev/null ; then
@@ -86,8 +69,6 @@ do
         echo ""
     fi
 done
-
-echo ""
 
 echo "Local configuration override files loaded:"
 echo $LOCAL_CONFIG_OVERRIDES | tr : "\n"
