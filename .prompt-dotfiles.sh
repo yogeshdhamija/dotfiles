98732a176c8c1b2881a0e563bf10ec5fa35aaeef
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 16:11:49 -0400

    prompt notifies if dotfiles unpushed

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -1,5 +1,14 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
 changes=$(dotfiles status --porcelain)
-if [[ ${changes} ]]; then
-	echo "dotfiles uncommitted local changes"
+unpushed=$(dotfiles remote show origin | grep 'master pushes to master')
+
+if [[ "${changes}" || "${unpushed}" ]]; then
+	printf '%s' "dotfiles"
+fi
+if [[ "${changes}" ]]; then
+	printf '%s' " uncommitted"
+fi
+if [[ "${unpushed}" ]]; then
+	printf '%s' " unpushed"
 fi
