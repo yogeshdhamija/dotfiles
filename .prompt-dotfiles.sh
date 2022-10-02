f7f7c707868848cc4b8b5a8294db793a5ddbc4e9
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 16:28:44 -0400

    prompt notifies if unpulled dotfiles changes

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -3,6 +3,7 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 allgood=1
 changes=$(dotfiles status --porcelain)
 pushed=$(dotfiles remote show origin | grep 'master pushes to master (up to date)')
+unpulled=$(dotfiles remote show origin | grep 'master pushes to master (local out of date)')
 
 printf '%s' "dotfiles"
 if [[ "${changes}" ]]; then
@@ -11,6 +12,10 @@ if [[ "${changes}" ]]; then
 fi
 if [[ ! "${pushed}" ]]; then
 	printf '%s' " unpushed"
+	allgood=0
+fi
+if [[ "${unpulled}" ]]; then
+	printf '%s' " unpulled"
 	allgood=0
 fi
 if [[ "${allgood}" == 1 ]]; then
