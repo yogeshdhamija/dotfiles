4bc7672aae89b7863ba7135459f267ace852184a
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 16:31:44 -0400

    prompt notifies on uncommitted, unpushed, and unpulled

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -1,10 +1,8 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-
-
 allgood=1
 changes=$(dotfiles status --porcelain)
-pushed=$(dotfiles remote show origin | grep 'master pushes to master (up to date)')
+unpushed=$(dotfiles remote show origin | grep 'master pushes to master (fast-forwardable)')
 unpulled=$(dotfiles remote show origin | grep 'master pushes to master (local out of date)')
 
 printf '%s' "dotfiles"
@@ -12,7 +10,7 @@ if [[ "${changes}" ]]; then
 	printf '%s' " uncommitted"
 	allgood=0
 fi
-if [[ ! "${pushed}" ]]; then
+if [[ "${unpushed}" ]]; then
 	printf '%s' " unpushed"
 	allgood=0
 fi
