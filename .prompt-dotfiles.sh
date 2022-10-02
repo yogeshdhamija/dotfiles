7279eaaf5018f84c096b26077126f4506e02e774
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 16:20:14 -0400

    prompt messaging of dotfiles status improved

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -1,12 +1,18 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+allgood=1
 changes=$(dotfiles status --porcelain)
 pushed=$(dotfiles remote show origin | grep 'master pushes to master (up to date)')
 
 printf '%s' "dotfiles"
 if [[ "${changes}" ]]; then
 	printf '%s' " uncommitted"
+	allgood=0
 fi
 if [[ ! "${pushed}" ]]; then
 	printf '%s' " unpushed"
+	allgood=0
+fi
+if ${allgood}; then
+	printf '%s' " all good"
 fi
