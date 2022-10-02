8340021a57182f2aafe9390f878dfc33abb4fce0
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 18:18:36 -0400

    environment check, removed node

--- .check_environment.sh
+++ .check_environment.sh
@@ -9,10 +9,6 @@ declare -a EXECS=(
     "exa"
     "jq"
     "yq"
-    "pandoc"
-    "pdflatex"
-    "node"
-    "npm"
 )
 declare -a NAMES=(
     "Neovim"
@@ -23,10 +19,6 @@ declare -a NAMES=(
     "Exa"
     "jq"
     "yq"
-    "Pandoc"
-    "TeX Live"
-    "node"
-    "npm"
 )
 declare -a ADDITIONAL1=(
     "Widely used rewrite of Vim that has some new features"
@@ -37,10 +29,6 @@ declare -a ADDITIONAL1=(
     "Like ls, but better"
     "JSON Parsing CLI tool"
     "YAML and XML Parsing CLI tool"
-    "Document converter, vim uses it for .md -> .pdf"
-    "Document converter, vim uses it for .md -> .pdf"
-    "JS runtime, vim uses it to run coc.nvim plugin (https://github.com/neoclide/coc.nvim), used for LSP"
-    "JS package manager, vim uses it to install coc.nvim plugin (https://github.com/neoclide/coc.nvim), used for LSP"
 )
 declare -a ADDITIONAL2=(
     "https://neovim.io/"
@@ -51,10 +39,6 @@ declare -a ADDITIONAL2=(
     "https://github.com/ogham/exa"
     "https://stedolan.github.io/jq/"
     "https://github.com/kislyuk/yq"
-    "https://pandoc.org/"
-    "https://www.tug.org/texlive/ | apt: texlive texlive-latex-extra | BasicTeX mac package"
-    "https://nodejs.org/"
-    ""
 )
 IFS=':' read -r -a local_config_overrides_loaded <<< "$LOCAL_CONFIG_OVERRIDES_LOADED"
 IFS=':' read -r -a local_config_overrides_not_loaded <<< "$LOCAL_CONFIG_OVERRIDES_NOT_LOADED"
@@ -72,6 +56,7 @@ do
         echo '    -' ${NAMES[$i-1]} '('${EXECS[$i-1]}')'
     fi
 done
+echo '   ---'
 
 echo "Programs not found in PATH:" 
 for (( i=1; i<${programs_arraylength}+1; i++ ));
@@ -83,6 +68,7 @@ do
         echo ""
     fi
 done
+echo '   ---'
 
 echo "Local configuration override files loaded:"
 printf '    %s\n' "${local_config_overrides_loaded[@]}"
