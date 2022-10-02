1a38926e2a757c3954533e34aed261ebf7b86be0
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-02 16:46:01 -0400

    Revert "prompt is faster"
    This reverts commit f8a229b64a91fad0c21ede5e66b8c29c919f54cf.

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -1,33 +1,23 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-check(){
-	echo "" > '/tmp/dotfiles-status.txt'
+allgood=1
+changes=$(dotfiles status --porcelain)
+unpushed=$(dotfiles remote show origin | grep 'master pushes to master (fast-forwardable)')
+unpulled=$(dotfiles remote show origin | grep 'master pushes to master (local out of date)')
 
-	allgood=1
-	changes=$(dotfiles status --porcelain)
-	unpushed=$(dotfiles remote show origin | grep 'master pushes to master (fast-forwardable)')
-	unpulled=$(dotfiles remote show origin | grep 'master pushes to master (local out of date)')
-
-	printf '%s' "dotfiles" >> '/tmp/dotfiles-status.txt'
-	if [[ "${changes}" ]]; then
-		printf '%s' " uncommitted" >> '/tmp/dotfiles-status.txt'
-		allgood=0
-	fi
-	if [[ "${unpushed}" ]]; then
-		printf '%s' " unpushed" >> '/tmp/dotfiles-status.txt'
-		allgood=0
-	fi
-	if [[ "${unpulled}" ]]; then
-		printf '%s' " unpulled" >> '/tmp/dotfiles-status.txt'
-		allgood=0
-	fi
-	if [[ "${allgood}" == 1 ]]; then
-		printf '%s' " all good" >> '/tmp/dotfiles-status.txt'
-	fi
-}
-
-if [[ $((1 + $RANDOM % 10)) -eq 9 ]]; then
-	check &
+printf '%s' "dotfiles"
+if [[ "${changes}" ]]; then
+	printf '%s' " uncommitted"
+	allgood=0
 fi
-
-cat /tmp/dotfiles-status.txt
+if [[ "${unpushed}" ]]; then
+	printf '%s' " unpushed"
+	allgood=0
+fi
+if [[ "${unpulled}" ]]; then
+	printf '%s' " unpulled"
+	allgood=0
+fi
+if [[ "${allgood}" == 1 ]]; then
+	printf '%s' " all good"
+fi
