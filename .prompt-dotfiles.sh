b01185ec79a6f8e64438ed50b8d6b9f5d95bd0e6
Yogesh Dhamija <ydhamija96@gmail.com>
2022-10-04 12:57:23 -0400

    prompt notifies about check-environment

--- .prompt-dotfiles.sh
+++ .prompt-dotfiles.sh
@@ -1,12 +1,14 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias "dotfiles-check"="cd ~ && chmod +x .check_environment.sh && ./.check_environment.sh && cd -"
 
+printf '%s' "(dotfiles"
 allgood=1
+
 changes=$(dotfiles status --porcelain)
 output=$(dotfiles remote show origin)
 unpushed=$(echo "${output}" | grep 'master pushes to master (fast-forwardable)')
 unpulled=$(echo "${output}" | grep 'master pushes to master (local out of date)')
 
-printf '%s' "(dotfiles"
 if [[ "${changes}" ]]; then
 	printf '%s' " uncommitted"
 	allgood=0
@@ -20,6 +22,17 @@ if [[ "${unpulled}" ]]; then
 	allgood=0
 fi
 if [[ "${allgood}" == 1 ]]; then
+	printf '%s' " ✓"
+fi
+printf '%s' ")"
+
+
+printf '%s' " (environment"
+need_programs=$(echo "$(dotfiles-check)" | tr '\n' '|' | tr -d ' ' | grep "ProgramsnotfoundinPATH:|.\+---|")
+
+if [[ "${need_programs}" ]]; then
+	printf '%s' " missing programs"
+else
 	printf '%s' " ✓"
 fi
 printf '%s' ")"
