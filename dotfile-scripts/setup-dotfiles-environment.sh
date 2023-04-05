20f68b725016ad0d71851bec55f17d2dfe4bdebb
Yogesh Dhamija <ydhamija96@gmail.com>
2023-04-05 10:50:44 -0400

    auto-adds submodule updates

--- dotfile-scripts/setup-dotfiles-environment.sh
+++ dotfile-scripts/setup-dotfiles-environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git clone --bare $DOTFILES_REPO_URL $HOME/.dotfiles
-alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+export DOTFILES_COMMAND='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfiles="${DOTFILES_COMMAND}"
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout && cd ~ && dotfiles submodule init && dotfiles submodule update
 source ~/.shell_aliases
 dotfiles-update
-cd ~ && chmod +x .check_environment.sh && ./.check_environment.sh
