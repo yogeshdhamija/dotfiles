18b2f3127a9693b28322c2aa468d352f215addfc
Yogesh Dhamija <ydhamija96@gmail.com>
2024-09-18 23:37:37 -0500

    idea beats code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,8 +34,8 @@ if status is-interactive
     # Set up my code editor preferences, in worst-to-best order:
     command -v vim > /dev/null && set -gx EDITOR "vim"
     command -v nvim > /dev/null && set -gx EDITOR "nvim"
-    command -v idea > /dev/null && set -gx EDITOR "idea --wait"
     command -v code > /dev/null && set -gx EDITOR "code -w"
+    command -v idea > /dev/null && set -gx EDITOR "idea --wait"
     command -v zed > /dev/null && set -gx EDITOR "zed -w"
 
     if test -f ~/.shell_aliases
