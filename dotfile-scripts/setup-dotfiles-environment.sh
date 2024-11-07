93a81c3c47c7ce4da66c705b2a5290c6a56e7215
Yogesh Dhamija <ydhamija96@gmail.com>
2024-11-06 20:00:53 -0600

    dotfiles repo tracks remote branches now

--- dotfile-scripts/setup-dotfiles-environment.sh
+++ dotfile-scripts/setup-dotfiles-environment.sh
@@ -4,6 +4,7 @@ git clone --bare $DOTFILES_REPO_URL $HOME/.dotfiles
 export DOTFILES_COMMAND='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dotfiles="${DOTFILES_COMMAND}"
 dotfiles config --local status.showUntrackedFiles no
+dotfiles config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" 
 dotfiles checkout && cd ~ && dotfiles submodule init && dotfiles submodule update
 source ~/.shell_aliases
 dotfiles-update
